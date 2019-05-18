d6d17d5e3a40f707e98cc5bf6360c7fb5cd24e4b
Alexander Lyashuk <crem@google.com>
2019-05-18 14:25:46 +0200

    No multiple rows insert in older sqlite3.

--- init.sql
+++ init.sql
@@ -1,17 +1,15 @@
-INSERT OR REPLACE INTO settings VALUES 
-('discord', 'token', '"secret!"'),
-('discord', 'botspam_ids', '[456137110609592325,579276060814868499]'),
-('discord', 'admin_role_ids', '[425664859410268163,4315458001123451003,571309511252508703]'),
-('cccc', 'enabled', 'true'),
-('cccc', 'cur_game', '-1'),
-('cccc', 'player_re', '"(?i)^lc0"'),
-('cccc', 'duck_url', '"https://lh3.googleusercontent.com/-NVa5Ms_Zj9U/XGXx1FxVUWI/AAAAAAAAVOw/mLWgmdVeVaY3MmByIqby0WFYpqMhOCBdQCL0BGAYYCw/h540/2019-02-14.png"'),
-('cccc', 'message', '"**{player1}** vs **{player2}** just started! Go to <https://chess.com/cccc> and watch!\n(If you want to be notified, type **!follow** in <#456137110609592325> to get a <@&499276275135807529> role)"'),
-('cccc', 'autosuspend', '3000'),
-('cccc', 'suspend_until', '0'),
-('cccc', 'allow_back_to_back', 'true'),
-('cccc', 'channel_id', '430695662108278784'),
-('urls', 'url_prefix', '"https://lc0.org/"')
-;
+INSERT OR REPLACE INTO settings VALUES ('discord', 'token', '"secret!"');
+INSERT OR REPLACE INTO settings VALUES ('discord', 'botspam_ids', '[456137110609592325,579276060814868499]');
+INSERT OR REPLACE INTO settings VALUES ('discord', 'admin_role_ids', '[425664859410268163,4315458001123451003,571309511252508703]');
+INSERT OR REPLACE INTO settings VALUES ('cccc', 'enabled', 'true');
+INSERT OR REPLACE INTO settings VALUES ('cccc', 'cur_game', '-1');
+INSERT OR REPLACE INTO settings VALUES ('cccc', 'player_re', '"(?i)^lc0"');
+INSERT OR REPLACE INTO settings VALUES ('cccc', 'duck_url', '"https://lh3.googleusercontent.com/-NVa5Ms_Zj9U/XGXx1FxVUWI/AAAAAAAAVOw/mLWgmdVeVaY3MmByIqby0WFYpqMhOCBdQCL0BGAYYCw/h540/2019-02-14.png"');
+INSERT OR REPLACE INTO settings VALUES ('cccc', 'message', '"**{player1}** vs **{player2}** just started! Go to <https://chess.com/cccc> and watch!\n(If you want to be notified, type **!follow** in <#456137110609592325> to get a <@&499276275135807529> role)"');
+INSERT OR REPLACE INTO settings VALUES ('cccc', 'autosuspend', '3000');
+INSERT OR REPLACE INTO settings VALUES ('cccc', 'suspend_until', '0');
+INSERT OR REPLACE INTO settings VALUES ('cccc', 'allow_back_to_back', 'true');
+INSERT OR REPLACE INTO settings VALUES ('cccc', 'channel_id', '430695662108278784');
+INSERT OR REPLACE INTO settings VALUES ('urls', 'url_prefix', '"https://lc0.org/"');
 
 
